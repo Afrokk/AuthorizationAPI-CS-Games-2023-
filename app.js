a8fc4d1b1e7260487471cac36ccf9758b23b2a6c
Afrokk <afrasiyabkhan379@gmail.com>
2023-03-18 15:57:44 -0400

    setup forms

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 import express from "express";
 import path from "path";
-import { fileURLToPath } from "url";
+import { fileURLToPath } from "url"; 
+import axios from "axios";
+import { stringify } from "querystring";
 
 const app = express();
-const __filename = fileURLToPath(import.meta.url);
+const __filename = fileURLToPath(import.meta.url);      
 const __dirname = path.dirname(__filename);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -17,14 +19,26 @@ app.get("/login", (req, res) => {
   res.render("login.ejs");
 });
 
-app.post("/login", (req, res) => {
-    res.send(req.body);
+app.post('/login', (req, res) => {
+    axios.post('https://idserver.lostgit.xyz/connect/token', { 
+        username: req.body.username,
+        password: req.body.password
+    }, {
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        }})
+    .then((response) => {
+        res.send(response.data);
+    })
+    .catch((error) => {
+        res.send(error);
+    });
+});
+
+app.listen(8000, () => {
+    console.log('Server running on port 3000');
 });
 
 app.get("/signup", (req, res) => {
     res.render("signup.ejs");
   });
-
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
-});
