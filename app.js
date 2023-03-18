dab752d2e1c7bb7cbab267f4c2832bb68adaeacf
Afrokk <afrasiyabkhan379@gmail.com>
2023-03-18 14:51:32 -0400

    setup login/signup

--- app.js
+++ app.js
@@ -5,13 +5,25 @@ import { fileURLToPath } from "url";
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 
-// Base route
 app.get("/", (req, res) => {
-  res.render("index");
+    res.render("index.ejs",);
 });
+
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
+});
+
+app.post("/login", (req, res) => {
+    res.send(req.body);
+});
+
+app.get("/signup", (req, res) => {
+    res.render("signup.ejs");
+  });
 
 app.listen(3000, () => {
     console.log("Server running on port 3000");
