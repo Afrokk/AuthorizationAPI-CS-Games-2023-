0f7f8d22ce76285bcb8bc982e5effc4955636c23
Afrokk <afrasiyabkhan379@gmail.com>
2023-03-19 12:31:49 -0400

    add authentication

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 import axios from "axios";
 import bodyParser from "body-parser";
+import { request } from "http";
 
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
@@ -12,11 +13,11 @@ app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  res.render("index.ejs", { jwt: app.get("jwt") });
 });
 
 app.get("/login", (req, res) => {
-  res.render("login.ejs", { jwt: null });
+  res.render("login.ejs", { jwt: app.get("jwt") });
 });
 
 app.post("/login", (req, res) => {
@@ -33,8 +34,8 @@ app.post("/login", (req, res) => {
         },
         })
         .then((response) => {
-        let jwt = response.data.access_token;
-        res.render("login.ejs", { jwt: jwt });
+        app.set('jwt', response.data.access_token);
+        res.render("login.ejs", { jwt: app.get('jwt') });
         })
         .catch((error) => {
         res.render("error.ejs", { errorMsg: "Invalid Username/Password" });
@@ -42,7 +43,7 @@ app.post("/login", (req, res) => {
 });
 
 app.get("/signup", (req, res) => {
-  res.render("signup.ejs");
+  res.render("signup.ejs", { sub: null });
 });
 
 app.post("/signup", (req, res) => {
@@ -50,26 +51,27 @@ app.post("/signup", (req, res) => {
     .post(
       "https://idserver.lostgit.xyz/user/create",
       {
-        client_id: "tenant1",
-        client_secret: "dev",
-        grant_type: "client_credentials",
-        username: req.body.username,
         password: req.body.password,
-        Authorization:
-          "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IlpocHhFQVlPVHFCVnhvTXpKWFRZc1ljRTItZz0iLCJ4NXQiOiJaaHB4RUFZT1RxQlZ4b016SlhUWXNZY0UyLWc9In0.eyJjbGllbnRfaWQiOiJ0ZW5hbnQxIiwiaXNzIjoiaHR0cHM6Ly9pZHNlcnZlci5sb3N0Z2l0Lnh5eiIsImF1ZCI6WyJodHRwczovL2lkc2VydmVyLmxvc3RnaXQueHl6IiwicmVwb2FwaSIsImlkc2VydmVyIiwic25pcHBldHNhcGkiXSwic2NvcGUiOlsicmVwb2FwaSIsImlkc2VydmVyIiwic25pcHBldHNhcGkiXSwibmJmIjoxNjc5MTYyNTQxLCJleHAiOjE2NzkxNjYxNDEsImlzX3NlcnZpY2UiOiJ0cnVlIiwicm9sZSI6InNlcnZpY2UifQ.bVMQghaDVXAwr1eRo-Q_fzri7NWyR_q6u0_tZ0P1_Y-r3rdqeWE7lR_Z87HUgbEkRGVTx5WgLS6WLSKNhzGSzPNzOZ2XDme2Xav3ties6maTUFrLAgpllg-KvjgBdBYlXGFCBu0aup0MsDaPwKQF7AzZvfNqbbOkXs9ATTzlc0SP5z85EKZkP-EjdgIDPcZsveITMCcSqa1C_JnizF7WnGjmB7M5d4h6dnKlZObluGyzNcLlThe4cjrJAVgRYlVrpr0g1N8t88k0y4IjJEh-cufDPCCJ8PdV255aCQNg8tDMkP3l8aLN_Fw8F3RxwfOTKfqtQZYZiVEkG5kJu2n9cA",
+        username: req.body.username,
       },
       {
         headers: {
           "Content-Type": "application/x-www-form-urlencoded",
+          "Authorization": "Bearer " + app.get('jwt')
         },
       }
     )
     .then((response) => {
-      res.send(response.data);
+      res.render("signup.ejs", { sub: 1 });
     })
     .catch((error) => {
-      res.render("error.ejs");
+      res.render("error.ejs", { errorMsg: error.message });
     });
+});
+
+app.get("/logout", (req, res) => {
+    app.set('jwt', null);
+    res.render("index.ejs", { jwt: app.get('jwt')});
 });
 
 app.listen(3000, () => {
